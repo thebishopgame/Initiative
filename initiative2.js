fe200f00e8e9f7cda6930026461dd00ef21ad3eb
thebishopgame <Igor@Kronos.local>
2014-05-13 00:27:59 -0700

    Almost all feautres done in React

--- initiative2.js
+++ initiative2.js
@@ -3,8 +3,8 @@
 
 function compareInit(a,b)
 {
-    var aInit = a.init + a.bonus;
-    var bInit = b.init + b.bonus;
+    var aInit = parseInt(a.init) + parseInt(a.bonus);
+    var bInit = parseInt(b.init) + parseInt(b.bonus);
     
     if (aInit < bInit)
         return 1;
@@ -25,6 +25,7 @@ var addCharBar = React.createClass({
     handleSubmit: function() {
         this.props.onAddChar(this.refs.charNameInput.getDOMNode().value);
         this.refs.charNameInput.getDOMNode().value = "";
+        this.refs.charNameInput.getDOMNode().focus();
         return false;
     },
     render: function() {
@@ -42,8 +43,8 @@ var addCharBar = React.createClass({
 // a row in the inQueue representing a single character in initiative
 var inChar = React.createClass({
     handleUp: function() {
-        this.props.onCharUp(this.props.id);
         this.refs.up.getDOMNode().blur();
+        this.props.onCharUp(this.props.id);
         return false;
     },
     handleDown: function() {
@@ -70,14 +71,21 @@ var inChar = React.createClass({
         this.props.onInitChange(this.props.id, this.refs.init.getDOMNode().value);
     },
     handleBonusChange: function() {
-        this.props.onDexChange(this.props.id, this.refs.bonus.getDOMNode().value);
+        this.props.onBonusChange(this.props.id, this.refs.bonus.getDOMNode().value);
     },
     handleDexChange: function() {
-        this.props.onBonusChange(this.props.id, this.refs.dex.getDOMNode().value);
+        this.props.onDexChange(this.props.id, this.refs.dex.getDOMNode().value);
     },
     render: function() {
+        var actClass;
+        
+        if (this.props.inInit && (this.props.id === this.props.active))
+            actClass = "active";
+        else
+            actClass = "";
+        
         return (
-            <li>{this.props.charName}
+            <li className={actClass}>{this.props.charName}
                 <div className="charControls">
                     <button ref="up" onClick={this.handleUp}>
                         <img className="controlimg" src="img/uparrow.png" />
@@ -104,7 +112,7 @@ var inChar = React.createClass({
                            min="0" 
                            max="99" 
                            className="initInput" 
-                           defaultValue="10"/>
+                           value={this.props.init}/>
                     <label className="inputLabelLong">Bonus</label>
                     <input type="number"
                            onChange={this.handleBonusChange}
@@ -113,7 +121,7 @@ var inChar = React.createClass({
                            min="-99" 
                            max="99" 
                            className="initInput" 
-                           defaultValue="0"/>
+                           value={this.props.bonus}/>
                     <label className="inputLabel">Dex</label>
                     <input type="number"
                            onChange={this.handleDexChange}
@@ -122,7 +130,7 @@ var inChar = React.createClass({
                            min="0" 
                            max="99" 
                            className="initInput" 
-                           defaultValue="10"/>
+                           value={this.props.dex}/>
                 </div>
             </li>
         );
@@ -143,10 +151,10 @@ var outChar = React.createClass({
         this.props.onInitChange(this.refs.init.getDOMNode().value);
     },
     handleBonusChange: function() {
-        this.props.onDexChange(this.refs.bonus.getDOMNode().value);
+        this.props.onBonusChange(this.refs.bonus.getDOMNode().value);
     },
     handleDexChange: function() {
-        this.props.onBonusChange(this.refs.dex.getDOMNode().value);
+        this.props.onDexChange(this.refs.dex.getDOMNode().value);
     },
     render: function() {
         return (
@@ -168,7 +176,7 @@ var outChar = React.createClass({
                            min="0" 
                            max="99" 
                            className="initInput" 
-                           value="10"/>
+                           value={this.props.init}/>
                     <label className="inputLabelLong">Bonus</label>
                     <input type="number"
                            onChange={this.handleBonusChange}
@@ -177,7 +185,7 @@ var outChar = React.createClass({
                            min="-99" 
                            max="99" 
                            className="initInput" 
-                           value="0"/>
+                           value={this.props.bonus}/>
                     <label className="inputLabel">Dex</label>
                     <input type="number"
                            onChange={this.handleDexChange}
@@ -186,7 +194,7 @@ var outChar = React.createClass({
                            min="0" 
                            max="99" 
                            className="initInput" 
-                           value="10"/>
+                           value={this.props.dex}/>
                 </div>
             </li>
         );
@@ -195,10 +203,38 @@ var outChar = React.createClass({
 
 // the main in-initiative queue
 var inQueueCmpt = React.createClass({
+    handleGen: function() {
+        this.refs.gen.getDOMNode().blur();
+        this.props.onGen();
+    },
+    handleSort: function() {
+        this.refs.sort.getDOMNode().blur();
+        this.props.onSort();
+    },
+    handleStartStop:function() {
+        this.refs.startstop.getDOMNode().blur();
+        this.props.onStartStop();
+    },
+    handleNext:function() {
+        this.refs.next.getDOMNode().blur();
+        this.props.onNext();
+    },
+    handleReset: function() {
+        this.refs.reset.getDOMNode().blur();
+        this.props.onReset();
+    },
+    handleClear: function() {
+        this.refs.clear.getDOMNode().blur();
+        this.props.onClear();
+    },
     render: function() {
         var id = 0;
         var chars = this.props.charList.map(function(char) {
-            return <inChar id={id++} 
+            return <inChar id={id++}
+                           charName={char.charName}
+                           init={char.init}
+                           bonus={char.bonus}
+                           dex={char.dex}
                            onInitChange={this.props.onInitChange}
                            onBonusChange={this.props.onBonusChange}
                            onDexChange={this.props.onDexChange}
@@ -207,29 +243,44 @@ var inQueueCmpt = React.createClass({
                            onCharHold={this.props.onCharHold}
                            onCharDel={this.props.onCharDel} 
                            onCharPause={this.props.onCharPause}
-                           charName={char.charName}/>;
+                           active={this.props.active}
+                           inInit={this.props.inInit}/>;
         }.bind(this));
+        
+        var startstop = this.props.inInit ? "Stop" : "Start";
         
         return (
             <div id="inQueue" className="queue">
                 <ol>{chars}</ol>
                 <div className='initControl'>
-                    <button className='initControlButton' onClick={this.handleGen}>
+                    <button className='initControlButton'
+                            ref="gen"
+                            onClick={this.handleGen}>
                         Generate
                     </button>&nbsp;&nbsp;
-                    <button className='initControlButton' onClick={this.handleSort}>
+                    <button className='initControlButton'
+                            ref="sort"
+                            onClick={this.handleSort}>
                         Sort
                     </button>&nbsp;&nbsp;
-                    <button className='initControlButton' onClick={this.handleStart}>
-                        Start
+                    <button className='initControlButtonStartStop'
+                            ref="startstop"
+                            onClick={this.handleStartStop}>
+                        {startstop}
                     </button>
-                    <button className='initControlButton' onClick={this.handleNext}>
+                    <button className='initControlButton'
+                            ref="next"
+                            onClick={this.handleNext}>
                         Next
                     </button>&nbsp;&nbsp;
-                    <button className='initControlButton' onClick={this.handleReset}>
+                    <button className='initControlButton'
+                            ref="reset"
+                            onClick={this.handleReset}>
                         Reset Init
                     </button>
-                    <button className='initControlButton' onClick={this.handleClear}>
+                    <button className='initControlButton'
+                            ref="clear"
+                            onClick={this.handleClear}>
                         Clear
                     </button>
                 </div>
@@ -250,7 +301,10 @@ var outQueueCmpt = React.createClass({
         
         var id = 0;
         var chars = this.props.charList.map(function(char) {
-            return <outChar id={id++} 
+            return <outChar id={id++}
+                            init={char.init}
+                            bonus={char.bonus}
+                            dex={char.dex}
                             onInitChange={this.props.onInitChange}
                             onBonusChange={this.props.onBonusChange}
                             onDexChange={this.props.onDexChange}
@@ -272,10 +326,10 @@ var outQueueCmpt = React.createClass({
 // main app class, also the state holder
 var app = React.createClass({
     getInitialState: function() {
-        if (localStorage.getItem("state") !== null)
+        if (localStorage.getItem("state") !== "")
             return JSON.parse(localStorage.state);
         else
-            return {inQueue: [], outQueue: []};
+            return {inQueue: [], outQueue: [], inInit: false, active: 0};
     },
     
     saveState: function() {
@@ -283,102 +337,72 @@ var app = React.createClass({
     },
     
     handleCharAdd: function(name) {
-        var queue = this.state.inQueue;
-        queue.push({charName:name, init:10, bonus:0, dex:10});
-        
-        this.setState({
-            inQueue: queue
-        });
-        
-        this.saveState();
+        if (name !== "") {    
+            var queue = this.state.inQueue;
+            queue.push({charName:name, init:10, bonus:0, dex:10});
+            this.setState({inQueue: queue}, this.saveState);
+        }
     },
     
     handleInitChangeIn: function(id, newInit) {
         var queue = this.state.inQueue;
 
         queue[id].init = newInit;
-        
-        this.setState({
-            inQueue: queue
-        });
-        
-        this.saveState();
+        this.setState({inQueue: queue}, this.saveState);
     },
     
     handleInitChangeOut: function(id, newInit) {
         var queue = this.state.outQueue;
         
         queue[id].init = newInit;
-        
-        this.setState({
-            outQueue: queue
-        });
-        
-        this.saveState();
+        this.setState({outQueue: queue}, this.saveState);
     },
     
     handleBonusChangeIn: function(id, newBonus) {
         var queue = this.state.inQueue;
         
         queue[id].bonus = newBonus;
-        
-        this.setState({
-            inQueue: queue
-        });
-        
-        this.saveState();
+        this.setState({inQueue: queue}, this.saveState);
     },
     
     handleBonusChangeOut: function(id, newBonus) {
         var queue = this.state.outQueue;
         
         queue[id].bonus = newBonus;
-        
-        this.setState({
-            outQueue: queue
-        });
-        
-        this.saveState();
+        this.setState({outQueue: queue}, this.saveState);
     },
     
     handleDexChangeIn: function(id, newDex) {
         var queue = this.state.inQueue;
         
         queue[id].dex = newDex;
-        
-        this.setState({
-            inQueue: queue
-        });
-        
-        this.saveState();
+        this.setState({inQueue: queue}, this.saveState);
     },
     
     handleDexChangeOut: function(id, newDex) {
         var queue = this.state.outQueue;
         
         queue[id].dex = newDex;
-        
-        this.setState({
-            outQueue: queue
-        });
-        
-        this.saveState();
+        this.setState({outQueue: queue}, this.saveState);
     },
     
     handleCharUp: function(id) {
         if (id > 0) {
             var queue = this.state.inQueue;
+            var act = this.state.active;
             
             var goingUp = queue[id];
             queue[id] = queue[id-1];
             queue[id-1] = goingUp;
             
+            if (this.state.inInit)
+                act--;
+                
             this.setState({
-                inQueue: queue
-            });
+                inQueue: queue,
+                active: act
+            }, this.saveState);
         }
-        
-        this.saveState();
     },
     
     handleCharDown: function(id) {
@@ -389,12 +413,8 @@ var app = React.createClass({
             queue[id] = queue[id+1];
             queue[id+1] = goingDown;
             
-            this.setState({
-                inQueue: queue
-            });
+            this.setState({inQueue: queue}, this.saveState);
         }
-        
-        this.saveState();
     },
     
     handleCharHold: function(id) {
@@ -403,67 +423,161 @@ var app = React.createClass({
         queue.push(queue[id]);
         queue.splice(id, 1);
         
-        this.setState({
-            inQueue: queue
-        });
-        
-        this.saveState();
-    },
-    
-    handleCharPause: function(id) {
-        var inqueue = this.state.inQueue;
-        var outqueue = this.state.outQueue;
-        
-        outqueue.push(inqueue[id]);
-        inqueue.splice(id, 1);
-        this.setState({
-            inQueue: inqueue,
-            outQueue: outqueue
-        });
-        
-        this.saveState();
-    },
-    
-    handleCharPlay: function(id) {
-        var inqueue = this.state.inQueue;
-        var outqueue = this.state.outQueue;
-        
-        inqueue.push(outqueue[id]);
-        outqueue.splice(id, 1);
-        this.setState({
-            inQueue: inqueue,
-            outQueue: outqueue
-        });
-        
-        this.saveState();
+        this.setState({inQueue: queue}, this.saveState);
     },
     
     handleCharDelIn: function(id) {
         var queue = this.state.inQueue;
+        var act = this.state.active;
+        
         queue.splice(id, 1);
         
+        if (act >= this.state.inQueue.length)
+            act = 0;
+            
         this.setState({
-            inQueue: queue
-        });
-        
-        this.saveState();
+            inQueue: queue,
+            active: act
+        }, this.saveState);
     },
     
     handleCharDelOut: function(id) {
         var queue = this.state.outQueue;
         queue.splice(id, 1);
         
+        this.setState({outQueue: queue}, this.saveState);
+    },
+    
+    handleCharPause: function(id) {
+        var inQ = this.state.inQueue;
+        var outQ = this.state.outQueue;
+        var act = this.state.active;
+        
+        outQ.push(inQ[id]);
+        inQ.splice(id, 1);
+        
+        if (act >= this.state.inQueue.length)
+            act = 0;
+        
         this.setState({
-            outQueue: queue
+            inQueue: inQ,
+            outQueue: outQ,
+            active: act
+        }, this.saveState);
+    },
+    
+    handleCharPlay: function(id) {
+        var inQ = this.state.inQueue;
+        var outQ = this.state.outQueue;
+        
+        inQ.push(outQ[id]);
+        outQ.splice(id, 1);
+        
+        this.setState({
+            inQueue: inQ,
+            outQueue: outQ
+        }, this.saveState);
+    },
+    
+    handleSort: function() {
+        var queue = this.state.inQueue;
+        
+        queue.sort(compareInit);
+        this.setState({
+            inQueue: queue,
+            inInit: false,
+            active: 0
+        }, this.saveState);
+    },
+    
+    handleGen: function() {
+        var inQ = this.state.inQueue;
+        var outQ = this.state.outQueue;
+        
+        for(var i=0; i<inQ.length; i++)
+            inQ[i].init = Math.floor((Math.random() * 20) + 1);
+        for(i=0; i<outQ.length; i++)
+            outQ[i].init = Math.floor((Math.random() * 20) + 1);
+            
+        this.handleSort();
+        
+        this.setState({
+            inQueue: inQ,
+            outQueue: outQ,
+            inInit: false,
+            active: 0
+        }, this.saveState);
+        
+    },
+    
+    handleStartStop: function() {
+        if (this.state.inQueue.length > 0) {
+            var state = !(this.state.inInit);
+            if (state)
+                this.setState({inInit: state}, this.saveState);
+            else
+                this.setState({inInit: state, 
+                               active: 0}, this.saveState);
+        }
+    },
+    
+    handleNext: function() {
+        var act = this.state.active;
+        
+        if (this.state.inInit) {
+            act++;
+            if (act >= this.state.inQueue.length)
+                act = 0;
+                
+            this.setState({active: act}, this.saveState);
+        }
+    },
+    
+    handleReset: function() {
+        var inQ = this.state.inQueue;
+        var outQ = this.state.outQueue;
+        
+        for(var i=0; i<inQ.length; i++)
+            inQ[i].init = 10;
+        for(i=0; i<outQ.length; i++)
+            outQ[i].init = 10;
+            
+        this.setState({
+            inQueue: inQ,
+            outQueue: outQ,
+            inInit: false,
+            active: 0
         });
         
         this.saveState();
     },
+    
+    handleClear: function() {
+        var inQ = this.state.inQueue;
+        var outQ = this.state.outQueue;
+        
+        while(inQ.length > 0)
+            inQ.pop();
+        while(outQ.length > 0)
+            outQ.pop();
+            
+        this.setState({
+            inQueue: inQ,
+            outQueue: outQ,
+            inInit: false,
+            active: 0
+        });
+
+        this.saveState();
+    },
+    
     render: function() {
         return (
             <div>
                 <addCharBar onAddChar={this.handleCharAdd} />
-                <inQueueCmpt charList={this.state.inQueue} 
+                <inQueueCmpt charList={this.state.inQueue}
+                             inInit={this.state.inInit}
+                             active={this.state.active}
                              onInitChange={this.handleInitChangeIn}
                              onBonusChange={this.handleBonusChangeIn}
                              onDexChange={this.handleDexChangeIn}
@@ -472,7 +586,12 @@ var app = React.createClass({
                              onCharHold={this.handleCharHold}
                              onCharDel={this.handleCharDelIn}
                              onCharPause={this.handleCharPause}
-                             onSort={this.handleSort}/>
+                             onSort={this.handleSort}
+                             onGen={this.handleGen}
+                             onStartStop={this.handleStartStop}
+                             onNext={this.handleNext}
+                             onReset={this.handleReset}
+                             onClear={this.handleClear}/>
                 <outQueueCmpt charList={this.state.outQueue}
                               onInitChange={this.handleInitChangeOut}
                               onBonusChange={this.handleBonusChangeOut}
